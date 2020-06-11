53fab066b9f25709126f484b54d07cab9c4ac815
davydovavi <47156002+davydovavi@users.noreply.github.com>
2020-06-11 23:01:42 +0400

    Add files via upload

---
+++ exercise2/exercise2/exercise2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = null;
+            
+            Console.WriteLine("User initialization");
+            try
+            {
+                user = ConsoleStreamer.UserReading();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            ConsoleStreamer.UserWriting();
+
+            Employee employee = null;
+            Console.WriteLine("Employee initialization");
+            try
+            {
+                employee = new ConsoleStreamer.EmployeeReading();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            ConsoleStreamer.EmployeeWriting();
+        }
+    }
+}
---
+++ exercise2/exercise2/exercise2/User.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise2
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+
+        public string SecondName { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public DateTime BirthDate { get; set; } = DateTime.Parse("01 01 2000");
+
+        private readonly int _age;
+
+        public User(string firstName, string secondName, string patronymic, DateTime birthDate)
+        {
+            FirstName = firstName;
+            SecondName = secondName;
+            Patronymic = patronymic;
+            BirthDate = birthDate;
+        }
+
+        public User(string firstName, string secondName, string patronymic)
+        {
+            FirstName = firstName;
+            SecondName = secondName;
+            Patronymic = patronymic;
+        }
+
+        public int Age
+        {
+            get
+            {
+                DateTime now = DateTime.Today;
+                int age = now.Year - BirthDate.Year;
+                if (BirthDate > now.AddYears(-age)) age--;
+                return age;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Firstname = {FirstName},\n Secondname = {SecondName}, \n Patronymic = {Patronymic}, \n Birthdate = {BirthDate}, \n Age = {Age}";
+        }
+
+    }
+}
