0ec068001c009c98e14065e3319876fee855a6a1
davydovavi <47156002+davydovavi@users.noreply.github.com>
2020-06-11 22:59:39 +0400

    Add files via upload

---
+++ exercise2/exercise2/exercise2/ConsoleStreamer.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise2
+{
+    public class ConsoleStreamer
+    {
+        static User user = null;
+        static Employee employee = null;
+
+        public static User UserReading()
+        {
+            Console.WriteLine("Enter firstname");
+            string firstName = Console.ReadLine();
+            if (firstName.Length > 0)
+            {
+                Console.WriteLine("Enter secondname");
+                string secondName = Console.ReadLine();
+                if (secondName.Length > 0)
+                {
+                    Console.WriteLine("Enter patronymic");
+                    string patronymic = Console.ReadLine();
+                    if (patronymic.Length > 0)
+                    {
+                        Console.WriteLine("Enter date of birth in standard format e.g. dd mm yyyy");
+                        if (DateTime.TryParse(Console.ReadLine(), out DateTime birthDate))
+                        {
+                            user = new User(firstName, secondName, patronymic, birthDate);
+                           
+                        }
+                        else
+                        {
+                            user = new User(firstName, secondName, patronymic);
+                           
+                        }
+                    }
+                }
+            }
+
+            return user;
+        }
+
+        public static Employee EmployeeReading()
+        {
+            Console.WriteLine("Enter firstname");
+            string firstName = Console.ReadLine();
+            if (firstName.Length > 0)
+            {
+                Console.WriteLine("Enter secondname");
+                string secondName = Console.ReadLine();
+                if (secondName.Length > 0)
+                {
+                    Console.WriteLine("Enter patronymic");
+                    string patronymic = Console.ReadLine();
+                    if (patronymic.Length > 0)
+                    {
+                        Console.WriteLine("Enter date of birth in standard format e.g. dd mm yyyy");
+                        if (DateTime.TryParse(Console.ReadLine(), out DateTime birthDate))
+                        {
+                            Console.WriteLine("Enter date work experience");
+                            if (int.TryParse(Console.ReadLine(), out int workexperience))
+                            {
+                                Console.WriteLine("Enter position");
+                                string position = Console.ReadLine();
+                                employee = new Employee(firstName, secondName, patronymic, birthDate, workexperience, position);
+                            }
+                                
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Enter date work experience");
+                            if (int.TryParse(Console.ReadLine(), out int workexperience))
+                            {
+                                Console.WriteLine("Enter position");
+                                string position = Console.ReadLine();
+                                employee = new Employee(firstName, secondName, patronymic, workexperience, position);
+                            }
+                                
+
+                        }
+                    }
+                }
+            }
+
+            return employee;
+        }
+
+        public static void UserWriting()
+        {
+            Console.WriteLine(user.ToString());
+        }
+
+        public static void EmployeeWriting()
+        {
+            Console.WriteLine(employee.ToString());
+        }
+    }
+}
---
+++ exercise2/exercise2/exercise2/Employee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise2
+{
+    public class Employee: User
+    {
+        private int _workExperience;
+
+        public string Position { get; set; }
+
+        public Employee(string firstName, string secondName, string patronymic, DateTime birthDate, int workExperience, string position)
+            :base(firstName,  secondName,  patronymic,  birthDate)
+        {
+            WorkExperience = workExperience;
+            Position = position;
+        }
+
+        public Employee(string firstName, string secondName, string patronymic, int workExperience, string position)
+            : base(firstName, secondName, patronymic)
+        {
+            WorkExperience = workExperience;
+            Position = position;
+        }
+
+        public int WorkExperience
+        {
+            set
+            {
+                if (value >= 0)
+                {
+                    _workExperience = value;
+                }
+                else
+                {
+                    throw new Exception("Work experience must be greater than 0");
+                }
+            }
+            get
+            {
+                return _workExperience;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Work experience = {WorkExperience}, position = {Position}";
+        }
+    }
+}
